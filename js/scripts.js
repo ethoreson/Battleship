ce282e0eedc1c12056d8fe513e5a4203076a1ffb
Emilie Thoreson and Joe Kramer <emilie+joe@>
2017-06-19 10:27:51 -0700

    initialize jquery on page load

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 //BACKEND
-function Ship(coordinates, size, indicator) {
-  this.coordinates = coordinates;
+function Ship(row, column, size, indicator, isVertical) {
+  this.row = row;
+  this.column = column;
+  this.isVertical = isVertical;
   this.size = size;
   this.indicator = indicator;
   this.sunk = false;
@@ -9,10 +11,30 @@ function Ship(coordinates, size, indicator) {
 function Player(turn) {
   this.turn = turn;
   this.totalHits = 0;
+  this.grid = new Grid.initializeGrid();
+}
+
+Player.prototype.getHitCount = function() {
+  return this.totalHits;
 }
 
 function Grid() {
   this.spaces = [];
+}
+
+Grid.prototype.initializeGrid() {
+  var spaces = [];
+  for(var r = 0; r < 10; r++) {
+    for(var c = 0; c < 10; c++) {
+      var space = new Space(r, c)
+      spaces.push(space);
+    }
+  }
+  return spaces;
+}
+
+Grid.prototype.placeShip = function(ship) {
+
 }
 
 function Space(row, column) {
@@ -27,6 +49,7 @@ function Space(row, column) {
 
 
 
-
-
 //FRONTEND
+$(document).ready(function() {
+  var player1 = new Player();
+});
