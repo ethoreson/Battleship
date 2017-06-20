3482d5c80890697e537138a85e01836cb783d46c
Anna Timofeeva <anjatimofeyva@gmail.com>
2017-06-20 15:12:38 -0700

    hide player 1 col

--- js/scripts.js
+++ js/scripts.js
@@ -48,6 +48,7 @@ Grid.prototype.initializeGrid = function() {
 }
 
 Player.prototype.checkPlacement = function(ship) {
+  console.log(ship.row);
   if (ship.isVertical === "vertical") {
     for(var i = 0; i < ship.size; i++) {
       if(ship.row + i > 9) {
@@ -116,6 +117,7 @@ var createTable = function(grid) {
 //FRONTEND
 $(document).ready(function() {
   var player1 = new Player(true);
+  var player2 = new Player(false);
 //  var battleship = new Ship(0, 0, 4, "b","vertical");
 
 
@@ -125,9 +127,6 @@ $(document).ready(function() {
     player1.markHit(id);
     $("#table").append(createTable(player1.grid));
   });
-
-
-
 
   $("#carrierButton1").click(function(event) {
     event.preventDefault();
@@ -223,17 +222,113 @@ $("#destroyerButton1").click(function(event) {
   }
 });
 
-$("#playbutton").click(function(event) {
+$("#player2setup").click(function(event) {
   event.preventDefault();
   $(".player1startupscreen").hide();
   $(".player2startupscreen").show();
+  $("#table").empty();
+  player1.turn = false;
+  player2.turn = true;
+});
+
+$("#carrierButton2").click(function(event) {
+  event.preventDefault();
+  var carrierRowString = $(".carrier_row").val();
+  var carrierRow = (parseInt(carrierRowString) - 1);
+  var carrierColumnString = $(".carrier_col").val();
+  var carrierColumn = (carrierColumnString.charCodeAt(0) - 65);
+  var carrierSize = 5;
+  var carrierIndicator = "carrier";
+  var hOrV = $("input:radio[name=carrierorientation]:checked").val();
+  var carrierShip = new Ship(carrierRow, carrierColumn, carrierSize, carrierIndicator, hOrV);
+  if (player2.checkPlacement(carrierShip)) {
+    player2.placeShip(carrierShip);
+    $("#table").append(createTable(player2.grid));
+  } else {
+    alert("Not enough room");
+  }
+
+});
+
+$("#battleshipButton2").click(function(event) {
+event.preventDefault();
+
+var battleshipRowString = $(".battleship_row").val();
+var battleshipRow = (parseInt(battleshipRowString) - 1);
+var battleshipColumnString = $(".battleship_col").val();
+var battleshipColumn = (battleshipColumnString.charCodeAt(0) - 65);
+var battleshipSize = 4;
+var battleshipIndicator = "battleship";
+var hOrV = $("input:radio[name=battleshiporientation]:checked").val();
+var battleshipShip = new Ship(battleshipRow, battleshipColumn, battleshipSize, battleshipIndicator, hOrV);
+if (player2.checkPlacement(battleshipShip)) {
+  player2.placeShip(battleshipShip);
+  $("#table").append(createTable(player2.grid));
+} else {
+  alert("Not enough room");
+}
+});
+
+$("#cruiserButton2").click(function(event) {
+event.preventDefault();
+
+var cruiserRowString = $(".cruiser_row").val();
+var cruiserRow = (parseInt(cruiserRowString) - 1);
+var cruiserColumnString = $(".cruiser_col").val();
+var cruiserColumn = (cruiserColumnString.charCodeAt(0) - 65);
+var cruiserSize = 3;
+var cruiserIndicator = "cruiser";
+var hOrV = $("input:radio[name=cruiserorientation]:checked").val();
+var cruiserShip = new Ship(cruiserRow, cruiserColumn, cruiserSize, cruiserIndicator, hOrV);
+if (player2.checkPlacement(cruiserShip)) {
+  player2.placeShip(cruiserShip);
+  $("#table").append(createTable(player2.grid));
+} else {
+  alert("Not enough room");
+}
+});
+
+$("#submarineButton2").click(function(event) {
+event.preventDefault();
+
+var submarineRowString = $(".submarine_row").val();
+var submarineRow = (parseInt(submarineRowString) - 1);
+var submarineColumnString = $(".submarine_col").val();
+var submarineColumn = (submarineColumnString.charCodeAt(0) - 65);
+var submarineSize = 3;
+var submarineIndicator = "submarine";
+var hOrV = $("input:radio[name=submarineorientation]:checked").val();
+var submarineShip = new Ship(submarineRow, submarineColumn, submarineSize, submarineIndicator, hOrV);
+if (player2.checkPlacement(submarineShip)) {
+  player2.placeShip(submarineShip);
+  $("#table").append(createTable(player2.grid));
+} else {
+  alert("Not enough room");
+}
+});
+
+$("#destroyerButton2").click(function(event) {
+event.preventDefault();
+var destroyerRowString = $(".destroyer_row").val();
+var destroyerRow = (parseInt(destroyerRowString) - 1);
+var destroyerColumnString = $(".destroyer_col").val();
+var destroyerColumn = (destroyerColumnString.charCodeAt(0) - 65);
+var destroyerSize = 2;
+var destroyerIndicator = "destroyer";
+var hOrV = $("input:radio[name=destroyerorientation]:checked").val();
+var destroyerShip = new Ship(destroyerRow, destroyerColumn, destroyerSize, destroyerIndicator, hOrV);
+if (player2.checkPlacement(destroyerShip)) {
+  player2.placeShip(destroyerShip);
+  $("#table").append(createTable(player2.grid));
+} else {
+  alert("Not enough room");
+}
 });
 
 $("#playbutton2").click(function(event) {
   event.preventDefault();
-  $(".player2startuscreen").hide();
+  $(".player2startupscreen").hide();
   $(".player1updates").show();
+  $("#table").empty();
 });
-
-
 });
